01ce8a7963b39797a5f91be2566b328229259bc6
unknown <mstrange@stanford.edu>
2019-11-04 20:20:32 -0800

    training

--- src/train.py
+++ src/train.py
@@ -23,38 +23,47 @@ def reformat_data(raw_path, comp_path, ignore_list, raw_array_path, comp_array_p
     
     for f in raw_files:
         electrode_num = None
-        if f.endswith('.mat') and f.split("_")[3] == "spike":
+        #if f.endswith('.mat') and f.split("_")[3] == "spike":
+        if f.endswith('.mat'):
             f = f.split(".")[0] # Get rid of file extension
             #print("Processing " + str(f))
             temp_inputs = loadmat(raw_path + str(f))
             electrode_num = int(f.split("_")[2])
             spike_num = int(f.split("_")[4])
+            spikedness = f.split("_")[3]
             input_arr = temp_inputs['temp']
             if electrode_num not in ignore_list:
-                raw_dict[(electrode_num, spike_num)] = input_arr.squeeze()
+                raw_dict[(electrode_num, spike_num, spikedness)] = input_arr.squeeze()
 
     for f in comp_files:
         electrode_num = None
-        if f.endswith('.mat') and f.split("_")[3] == "spike":
+      #  if f.endswith('.mat') and f.split("_")[3] == "spike":
+        if f.endswith('.mat'):
             f = f.split(".")[0] # Get rid of file extension
             #print("Processing " + str(f))
             temp_inputs = loadmat(comp_path + str(f))
             electrode_num = int(f.split("_")[2])
             spike_num = int(f.split("_")[4])
+            spikedness = f.split("_")[3]
             input_arr = temp_inputs['temp']
             if electrode_num not in ignore_list:
-                comp_dict[(electrode_num, spike_num)] = input_arr.squeeze()
+                comp_dict[(electrode_num, spike_num, spikedness)] = input_arr.squeeze()
+
 
     num_examples = len(raw_dict.keys())
-    length_example = len(raw_dict[(1,1)])
+
+    print(num_examples)
+
+    length_example = len(raw_dict[(1,1, "nonspike")])
+    length_example = len(raw_dict[(1,1, "spike")])
 
     raw_arr = np.zeros((length_example, num_examples))
     comp_arr = np.zeros((length_example, num_examples))
 
     i = 0
-    for (elec_num, spike_num) in raw_dict.keys():
+    for (elec_num, spike_num, spikedness) in raw_dict.keys():
         #print("Electrode: " + str(elec_num) + "\tSpike: " + str(spike_num))
-        if (elec_num, spike_num) not in comp_dict.keys():
+        if (elec_num, spike_num, spikedness) not in comp_dict.keys():
             print("FAILURE...not in comp_dict")
             break
         raw_arr[:,i] = raw_dict[(elec_num, spike_num)]
@@ -73,7 +82,9 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
     raw_array_path = "./raw_array.npy"
     comp_array_path = "./comp_array.npy"
 
-    if not os.path.exists(raw_array_path) or not os.path.exists(comp_array_path):
+    remake = True
+
+    if not os.path.exists(raw_array_path) or not os.path.exists(comp_array_path) or remake is True:
         print("np files don't exist, creating now...")
         reformat_data(raw_path, comp_path, ignore_list, raw_array_path, comp_array_path)
     else:
@@ -87,11 +98,21 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
 
    # raw_arr_norm = tf.keras.utils.normalize(raw_arr, axis=1)
     raw_arr_norm = raw_arr
-    #comp_arr_norm = comp_arr / 255
-    comp_arr_norm = comp_arr
+    comp_arr_norm = comp_arr / 255
+    #comp_arr_norm = comp_arr
 
     raw_arr_norm = raw_arr_norm.T
     comp_arr_norm = comp_arr_norm.T
+
+    # raw_arr_norm = raw_arr_norm[:, :, None]
+    # #print(raw_arr_norm.shape)
+    # comp_arr_norm = comp_arr_norm[ :, :, None ]
+
+    raw_arr_norm_backup = raw_arr_norm
+    comp_arr_norm_backup = comp_arr_norm
+
+    raw_arr_norm = raw_arr_norm[:, None, :]
+    comp_arr_norm = comp_arr_norm[:, None, :]
 
     raw_train = tf.dtypes.cast(raw_arr_norm[0:12000], tf.float32)
     raw_dev = tf.dtypes.cast(raw_arr_norm[12000:], tf.float32)
@@ -99,14 +120,17 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
     comp_dev = tf.dtypes.cast(comp_arr_norm[12000:], tf.float32)
 
     def model_create(example_length):
-        inputs = keras.Input(shape=(example_length,), name="in")
-        x = layers.Dense(1500, activation="relu", name="dense_1")(inputs)
-        x = layers.BatchNormalization(name="batch_norm_1")(x)
+        inputs = keras.Input(shape=(1, example_length, ), name="in")
+        x = layers.Conv1D(filters=10, kernel_size=5, strides=1, padding='same', activation=None,data_format='channels_first', use_bias=True, kernel_initializer='glorot_uniform')(inputs)
+      #  x = layers.Dense(1000, activation="relu", name="dense_1")(inputs)
+      #  x = layers.BatchNormalization(name="batch_norm_1")(x)
         x = layers.Dense(1500, activation="relu", name="dense_2")(x)
         x = layers.Dense(1500, activation="relu", name="dense_3")(x)
-        x = layers.BatchNormalization(name="batch_norm_2")(x)
+        x = layers.BatchNormalization(name="batch_norm_1")(x)
         x = layers.Dense(1500, activation="relu", name="dense_4")(x)
         x = layers.Dense(1500, activation="relu", name="dense_5")(x)
+        #x = layers.Conv1D(filters=1, kernel_size=3, strides=1, padding='same', activation=None,data_format='channels_first', use_bias=True, kernel_initializer='glorot_uniform')(inputs)
+        x = layers.Conv1D(filters=1, kernel_size=3, strides=1, padding='same', activation=None, data_format='channels_first', kernel_initializer='glorot_uniform')(x)
         #outputs = layers.Dense(example_length, activation="tanh", name="out")(x)
         outputs = layers.Dense(example_length, name="out")(x)
         return keras.Model(inputs=inputs, outputs=outputs)
@@ -146,13 +170,12 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
 
     if plot is True:
         which_num = plot_idx
-        comp_re = np.reshape(comp_train[which_num], (1, 71))
+        comp_re = np.reshape(comp_train[which_num], (1,1, 71))
         predicted = model.predict(comp_re)
         predicted = predicted.reshape((71,1))
-        actual = raw_train[which_num]
         plt.figure()
         plt.title('Compressed...')
-        plt.plot(comp_train[which_num])
+        plt.plot(comp_arr_norm_backup[which_num])
         if save is True:
             plt.savefig(f"../img/compressed_{which_num}.png")
         plt.figure()
@@ -162,7 +185,7 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
             plt.savefig(f"../img/reconstructed_{which_num}.png")
         plt.figure()
         plt.title('Raw...')
-        plt.plot(actual)
+        plt.plot(raw_arr_norm_backup[which_num])
         if save is True:
             plt.savefig(f"../img/actual_{which_num}.png")
         if show is True:
@@ -170,7 +193,7 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
 
 if __name__=="__main__":
     train(
-        use_chk=False, 
+        use_chk=True, 
         plot=True, 
         show=True,
         save=False,
