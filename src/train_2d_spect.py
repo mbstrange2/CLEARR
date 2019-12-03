6b43752d5a58e301706043c773ab545a09b1f493
AndrewWang <andyzywang@gmail.com>
2019-12-03 01:35:52 -0800

    Models now working; only using tf_recordz_0 so far, so rest of input flow needs to be added

--- src/train_2d_spect.py
+++ src/train_2d_spect.py
@@ -92,15 +92,14 @@ def parse_proto(example_proto):
     #                        tf.subtract(tf.reduce_max(raw_arr), tf.reduce_min(raw_arr))
     #                    )
     raw_arr_norm = raw_arr
-    comp_arr = comp_arr[None, :]
-    raw_arr_norm = raw_arr_norm[None, :]
-    raw_arr_kapre = raw_arr_norm[None,:,:]
-    pdb.set_trace()
-    raw_arr_stft = Spectrogram(n_dft=8, n_hop=1, padding='same', power_spectrogram=1.0,
-                               return_decibel_spectrogram=False, trainable_kernel=False,
-                               image_data_format='channels_last', name='stft_output')(raw_arr_kapre)  # (B x F x T x 1)
-    raw_arr_stft = raw_arr_stft[None, :]
-    return comp_arr, raw_arr_norm
+    comp_arr = comp_arr[:, None]    #model takes in B x T x 1
+    raw_arr_kapre = tf.pad(raw_arr_norm, [[3,4]])     #stft takes in B x T
+    raw_arr_stft = tf.abs(tf.signal.stft(raw_arr_kapre, frame_length=8, frame_step=1, fft_length=8))    #stft outputs B x T x F
+    raw_arr_stft = tf.transpose(raw_arr_stft, perm=[1,0])  #change to B x F x T x 1, same as Spectrogram layer output
+    raw_arr_stft = tf.expand_dims(raw_arr_stft, -1)
+    raw_arr_norm = raw_arr_norm[None, :]    #model outputs B x 1 x T
+    # pdb.set_trace()
+    return comp_arr, {"1d_output": raw_arr_norm, "stft_output": raw_arr_stft}
 
 # def create_inputs(newds, batch_size, model_spectrogram):
     # iterator = newds.__iter__()
@@ -135,15 +134,15 @@ def create_inputs(comp_arr, raw_arr):     #tensorflow forces tf_map function to 
 
 def model_create(example_length):
         inputs = keras.Input(shape=(example_length,1), name="in")
-        x = layers.Dense(1500, activation="relu", name="dense_1")(inputs)
+        x = layers.Conv1D(filters=64, kernel_size=5, strides=1, padding='same', activation='relu' ,data_format='channels_last', use_bias=True, kernel_initializer='glorot_uniform', name="conv_1")(inputs)
+        x = layers.Dense(1024, activation="relu", name="dense_2")(x)
         x = layers.BatchNormalization(name="batch_norm_1")(x)
-        x = layers.Dense(1500, activation="relu", name="dense_2")(x)
-        x = layers.Dense(1500, activation="relu", name="dense_3")(x)
+        x = layers.Dense(1024, activation="relu", name="dense_3")(x)
+        x = layers.Dense(1024, activation="relu", name="dense_4")(x)
         x = layers.BatchNormalization(name="batch_norm_2")(x)
-        x = layers.Dense(1500, activation="relu", name="dense_4")(x)
-        x = layers.Dense(1500, activation="relu", name="dense_5")(x)
-        #outputs = layers.Dense(example_length, activation="tanh", name="out")(x)
-        x = layers.Dense(example_length, name="dense_6")(x) #(B x T x T)
+        x = layers.Dense(1024, activation="relu", name="dense_5")(x)
+        x = layers.Dense(64, name="dense_6")(x) #(B x T x 64)
+        x = layers.Conv1D(filters=1, kernel_size=3, strides=1, padding='same', activation=None, data_format='channels_last', kernel_initializer='glorot_uniform', name="conv_2")(x)    #(B x T x 1)
         x = layers.Dense(1, name="dense_7")(x)  # (B x T x 1)
         x_1d = layers.Permute((2,1), name='1d_output')(x)    # (B x 1 x T) because this is format that kapre wants
         x_spect = Spectrogram(n_dft=8, n_hop=1, padding='same', power_spectrogram=1.0,
@@ -219,7 +218,7 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
         
     model = model_create(n_x)
     model.summary()
-    
+    pdb.set_trace()
     model_spectrogram = model_create_spectrogram(n_y)
     model_spectrogram.summary()
     
@@ -269,14 +268,14 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
     for i in range(num_use):
         final_files.append(f"D:\\Programs\\Python\\Projects\\CS230\\data\\tfrecords_new\\tf_records_{file_idx[i]}.tfrecordz")
         # Accumulate the number of examples in each file
-    #    total_size += int(size_arr[file_idx[i]])
+        # total_size += int(size_arr[file_idx[i]])
 
     print(f"Files for training: {final_files}")
-    #print(f"TOTAL NUMBER OF EXAMPLES: {total_size}")
+    print(f"TOTAL NUMBER OF EXAMPLES: {total_size}")
     
     newds = tf.compat.v2.data.TFRecordDataset(final_files, compression_type="GZIP", num_parallel_reads=16)
     buffer_size = 3000000
-    batch_size = 256
+    batch_size = 128
 
     val_iterator = newds.take(65536).__iter__()
     train_iterator = newds.skip(65536).__iter__()
@@ -316,7 +315,7 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
     
     if training is True:
         epochs = 3
-        STEPS_PER_EPOCH = math.floor(total_size / batch_size) - 100
+        STEPS_PER_EPOCH = 256
         checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
         callbacks_list = [checkpoint]
         pdb.set_trace()
@@ -332,33 +331,37 @@ def train(use_chk=False, plot=False, show=False, save=False, plot_idx=0):
         plt.savefig(f"D:\\Programs\\Python\\Projects\\CS230\\data\\loss_batch_size_{batch_size}_epochs_{epochs}.png")
         if show is True:
             plt.show()
-        #model.save_weights(".\\model_chk_Mel.hd5")
+        model.save_weights(".\\model_chk_Mel.hd5")
 
     if plot is True:
-        which_num = plot_idx
-        # test_input = comp_train_kapre[which_num].reshape((1,71,1))
-        # (predicted, _) = model.predict(test_input)
-        # print("Predicted shape = ", predicted.shape)
-        # predicted = predicted.reshape((71,1))
-        # actual = raw_train[which_num].reshape((71,1))
-        # plt.figure()
-        # plt.title('Compressed...')
-        # plt.plot(comp_train[which_num].reshape((71,1)))
-        # if save is True:
-            # plt.savefig("..\\img\\compressed_{which_num}.png")
-        # plt.figure()
-        # plt.title('Recon...')
-        # plt.plot(predicted)
-        # if save is True:
-            # plt.savefig("..\\img\\reconstructed_{which_num}.png")
-        # plt.figure()
-        # plt.title('Raw...')
-        # plt.plot(actual)
-        # if save is True:
-            # plt.savefig("..\\img\\actual_{which_num}.png")
-        # if show is True:
-            # plt.show()
-        #pdb.set_trace()
+        newds_iter = iter(newds)
+        example = next(newds_iter)
+        example_comp = example[0][plot_idx].numpy().reshape((1,71,1))
+        example_raw = example[1]['1d_output'][plot_idx].numpy()
+        example_stft = example[1]['stft_output'][plot_idx].numpy()
+        
+        (predicted, _) = model.predict(example_comp)
+        print("Predicted shape = ", predicted.shape)
+        predicted = predicted.reshape((71,1))
+        
+        plt.figure()
+        plt.title('Compressed...')
+        plt.plot(example_comp.reshape((71,1)))
+        if save is True:
+            plt.savefig("..\\img\\compressed_{which_num}.png")
+        plt.figure()
+        plt.title('Recon...')
+        plt.plot(predicted)
+        if save is True:
+            plt.savefig("..\\img\\reconstructed_{which_num}.png")
+        plt.figure()
+        plt.title('Raw...')
+        plt.plot(example_raw.reshape((71,1)))
+        if save is True:
+            plt.savefig("..\\img\\actual_{which_num}.png")
+        if show is True:
+            plt.show()
+        # pdb.set_trace()
         
         
         K.clear_session()
