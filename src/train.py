a3527de79412383b602d9941a4dd99d501650169
unknown <mstrange@stanford.edu>
2019-12-05 12:44:53 -0800

    specs of model

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 from scipy.io import loadmat
+from scipy.io import savemat
 import h5py
 import numpy as np
 from os import listdir
@@ -24,8 +25,6 @@ def filter_data_files(item):
         return True
     else:
         return False
-        
-tf.executing_eagerly()
 
 def truncate(f, n):
     '''Truncates/pads a float f to n decimal places without rounding'''
@@ -67,9 +66,6 @@ def create_test_set(raw_path, comp_path, ignore_list, array_path):
 
             if (starting != 0) and (starting != 5):
                 continue
-            #print(f"starting_minute is {starting}, electrode is {electrode_num}")
-            #continue
-
 
             if os.path.exists(compressed_path):
                 print(f"File number: {file_num}")
@@ -82,8 +78,8 @@ def create_test_set(raw_path, comp_path, ignore_list, array_path):
                     comp_input_collapsed = np.zeros((comp_input_np.shape[0], comp_input_np.shape[1]+1))
 
                     jj = 0
-                    for ii in range(comp_input_np.shape[0]):
-                        if (comp_input_np[ii][0] != 0): 
+                    for ii in range(comp_input_np.shape[0]) :
+                        if (comp_input_np[ii][0] != 0) and (np.ptp(comp_input_np[ii, 1:]) > 0): 
 
                             spike_num = comp_input_np[ii][0]
                             comp_input_collapsed[jj][0] = electrode_num
@@ -92,7 +88,6 @@ def create_test_set(raw_path, comp_path, ignore_list, array_path):
                             #print(f"Spike num = {spike_num}, electrode = {electrode_num}, spike is {comp_input_np[ii][1:]}")
                             jj = jj + 1
 
-    
                     comp_input_collapsed = comp_input_collapsed[0:jj, :]
                     comp_list.append(comp_input_collapsed)
 
@@ -102,7 +97,6 @@ def create_test_set(raw_path, comp_path, ignore_list, array_path):
 
                         for i in range(len(comp_list)):
                             temp_len = comp_list[i].shape[0]
-                            #print(f"LEN {i} : {temp_len}")
                             len_sum = len_sum + temp_len
                             lens.append(temp_len)
 
@@ -152,6 +146,7 @@ def create_test_set(raw_path, comp_path, ignore_list, array_path):
         else:
             # first array is the raw, second is the compressed
             np.savez_compressed(array_path + f"_{check_num}_test.npz", final_np_comp)
+
 
 def reformat_data(raw_path, comp_path, ignore_list, raw_array_path, comp_array_path):
     print_now = 1
@@ -205,7 +200,7 @@ def reformat_data(raw_path, comp_path, ignore_list, raw_array_path, comp_array_p
 
                     jj = 0
                     for ii in range(raw_input_np.shape[0]):
-                        if (raw_input_np[ii][0] != 0): # and (np.ptp(comp_input_np[ii, 1:]) > 2)): # and (np.sum(comp_input_np[ii, 1:] == 0) > 60):
+                        if (raw_input_np[ii][0] != 0) and (np.ptp(comp_input_np[ii, 1:]) > 0):
                             raw_input_collapsed[jj] = raw_input_np[ii][1:]
                             comp_input_collapsed[jj] = comp_input_np[ii][1:]
                             jj = jj + 1
@@ -275,8 +270,7 @@ def reformat_data(raw_path, comp_path, ignore_list, raw_array_path, comp_array_p
             # first array is the raw, second is the compressed
             np.savez_compressed(raw_array_path + f"_{check_num}.npz", (final_np_raw, final_np_comp))
 
-
-
+# Convert numpyz array to tfrecord files
 def array_to_tfrecords(comp, raw, output_file):
     with tf.io.TFRecordWriter(output_file, options="GZIP") as writer:
         for j in range(comp.shape[0]):
@@ -298,16 +292,37 @@ def parse_proto(example_proto):
     parsed_features = tf.io.parse_single_example(example_proto, features)
     comp_arr = parsed_features['comp']
     raw_arr = parsed_features['raw']
-    #comp_arr_norm = tf.math.l2_normalize(comp_arr)
-    #raw_arr_norm = tf.divide(tf.subtract(raw_arr, tf.reduce_min(raw_arr)), 
-    #                        tf.subtract(tf.reduce_max(raw_arr), tf.reduce_min(raw_arr))
-    #                    )
     raw_arr_norm = raw_arr
+    # Format data for CNN input format
     comp_arr = comp_arr[None, :]
     raw_arr_norm = raw_arr_norm[None, :]
     return comp_arr, raw_arr_norm
 
-def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, save=False, epochs=1, plot_idx=0):
+
+def model_create(example_length):
+
+    # Model P1
+    inputs = keras.Input(shape=(1, example_length, ), name="in")
+    #inputs = keras.Input(shape=(example_length, ), name="in")
+    #x = layers.Dense(2000, activation="relu", name="dense_1")(inputs)
+    x = layers.Conv1D(filters=32, kernel_size=5, strides=1, padding='valid', activation='relu', 
+            data_format='channels_first', use_bias=True, kernel_initializer='glorot_uniform')(inputs)
+    x = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
+    x = layers.Conv1D(filters=64, kernel_size=5, strides=1, padding='valid', activation='relu', data_format='channels_first', 
+            use_bias=True, kernel_initializer='glorot_uniform')(x)
+    x = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
+    x = layers.Conv1D(filters=96, kernel_size=3, strides=1, padding='valid', activation='relu', data_format='channels_first', 
+        use_bias=True, kernel_initializer='glorot_uniform')(x)
+    x = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
+    x = layers.Dense(2000, activation="relu", name="dense_4")(x)
+    # x = layers.Dense(1000, activation="relu", kernel_regularizer=keras.regularizers.l2(0.002), name="dense_4")(x)
+    x = layers.Dense(2000, activation="relu", name="dense_5")(x)
+    x = layers.Conv1D(filters=1, kernel_size=5, strides=1, padding='same', activation='relu', data_format='channels_first', use_bias=True, kernel_initializer='glorot_uniform')(x)
+
+    outputs = layers.Dense(example_length, name="out")(x)
+    return keras.Model(inputs=inputs, outputs=outputs)
+
+def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, save=False, epochs=1, plot_idx=0, val=False):
 
     raw_path = Path("C:/Users/Mek/DATA_TAR") 
     comp_path = Path("C:/Users/Mek/DATA_TAR")
@@ -350,7 +365,7 @@ def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, 
     # Shuffle the files to prevent grouping
     #random.shuffle(file_idx)
 
-    file_idx = [18] #[54, 10, 32]
+    file_idx = [0] #[54, 10, 32]
 
     final_files = []
     total_size = 0
@@ -361,46 +376,7 @@ def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, 
 
     print(f"Files for training: {final_files}")
     print(f"TOTAL NUMBER OF EXAMPLES: {total_size}")
-
-    newds = tf.compat.v2.data.TFRecordDataset(final_files, compression_type="GZIP", num_parallel_reads=16)
-    buffer_size = 3000000
-    batch_size = 256
-
-    newds = newds.map(parse_proto, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    #newds = newds.shuffle(buffer_size)
-    newds = newds.repeat()
-    newds = newds.batch(batch_size) #, drop_remainder=True)
-    newds = newds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-    #ds_it = iter(newds)
-
-    def model_create(example_length):
-
-        inputs = keras.Input(shape=(1, example_length, ), name="in")
-        #inputs = keras.Input(shape=(example_length, ), name="in")
-        #x = layers.Dense(2000, activation="relu", name="dense_1")(inputs)
-        x = layers.Conv1D(filters=32, kernel_size=5, strides=1, padding='valid', activation='relu', 
-                data_format='channels_first', use_bias=True, kernel_initializer='glorot_uniform')(inputs)
-        x = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
-        x = layers.Conv1D(filters=64, kernel_size=5, strides=1, padding='valid', activation='relu', data_format='channels_first', 
-                use_bias=True, kernel_initializer='glorot_uniform')(x)
-        x = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
-
-        x = layers.Conv1D(filters=96, kernel_size=3, strides=1, padding='valid', activation='relu', data_format='channels_first', 
-            use_bias=True, kernel_initializer='glorot_uniform')(x)
-        x = keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='valid')(x)
-      #  x = layers.Dense(1000, activation="relu", name="dense_1")(inputs)
-      #  x = layers.BatchNormalization(name="batch_norm_1")(x)
-        #x = layers.Dense(2000, activation="relu", name="dense_2")(x)
-        #x = layers.BatchNormalization(name="batch_norm_1")(x)
-        x = layers.Dense(2000, activation="relu", name="dense_4")(x)
-       # x = layers.Dense(1000, activation="relu", kernel_regularizer=keras.regularizers.l2(0.002), name="dense_4")(x)
-        #x = layers.BatchNormalization(name="batch_norm_2")(x)
-        x = layers.Dense(2000, activation="relu", name="dense_5")(x)
-        x = layers.Conv1D(filters=1, kernel_size=3, strides=1, padding='same', activation='relu', data_format='channels_first', use_bias=True, kernel_initializer='glorot_uniform')(x)
-        #x = layers.Flatten(data_format="channels_last")(x)
-        outputs = layers.Dense(example_length, name="out")(x)
-        return keras.Model(inputs=inputs, outputs=outputs)
-
+    # Create the model
     model = model_create(71)
 
     checkpoint_filepath = "./model_chk.hdf5"
@@ -413,6 +389,26 @@ def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, 
         print("Training model...")
         training = True
 
+    ##### TRAINING HYPERPARAMETERS #####
+    l_r = .002 # Default .001
+    b_1 = 0.85 # Default 0.9
+    b_2 = 0.999 # Default 0.999
+    batch_size = 512
+    ####################################
+
+    newds = tf.compat.v2.data.TFRecordDataset(final_files, compression_type="GZIP", num_parallel_reads=16)
+    buffer_size = 2000000
+
+    newds = newds.map(parse_proto, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    #newds = newds.shuffle(buffer_size)
+    newds = newds.repeat()
+    newds = newds.batch(batch_size) #, drop_remainder=True)
+    newds = newds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    num_train_samples = 400000
+
+    train_set = newds.take(num_train_samples)
+
     initial_learning_rate = 0.1
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate,
@@ -420,26 +416,32 @@ def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, 
         decay_rate=0.96,
         staircase=True)
 
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=.001),  # Optimizer
+    model.compile(
+        optimizer=keras.optimizers.Adam(
+            learning_rate=l_r,
+            beta_1=b_1,
+            beta_2=b_2
+            ),  # Optimizer
         # Loss function to minimize
         loss=keras.losses.MeanSquaredError(),
         # List of metrics to monitor
         metrics=[keras.metrics.MeanSquaredError()])
 
-
     #STEPS_PER_EPOCH = math.floor(total_size / batch_size) - 1
-    STEPS_PER_EPOCH = 256
+    STEPS_PER_EPOCH = math.floor(num_train_samples / batch_size) - 1
 
+    final_loss = -1
     if training is True:
         checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
         callbacks_list = [checkpoint]
-        history = model.fit(newds,
+        history = model.fit(train_set,
                    # batch_size=batch_size, # 128
                     steps_per_epoch=STEPS_PER_EPOCH,
                     epochs=epochs, # 100
                     # Have this callback for checkpointing our model
                     callbacks=callbacks_list)
         hist_loss = history.history['loss']
+        final_loss = hist_loss[-1]
         plt.figure()
         plt.title('Loss vs epoch...')
         plt.plot(hist_loss)
@@ -447,17 +449,15 @@ def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, 
         if show is True:
             plt.show()
 
-    newds_iter = iter(newds)
-    example = next(newds_iter)
+    train_iter = iter(train_set)
+    example = next(train_iter)
     #### PUMIAO + ANDREW - CHANGE THE SECOND INDEX
     # PIC
-
     pic = plot_idx
     example_comp = example[0][pic]
     example_raw = example[1][pic]
 
     if plot is True:
-
         #comp_re = np.reshape(example_comp, (1, 71))
         comp_re = np.reshape(example_comp, (1, 1, 71))
         predicted = model.predict(comp_re)
@@ -480,33 +480,59 @@ def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, 
         if show is True:
             plt.show()
 
-    #test_data = True
+    dev_loss = -1
+    if val is True:
+    # Evaluation on dev set
+        num_use = 1
+        file_idx = [20]
+        dev_files = []
+        dev_size = 0
+        for i in range(num_use):
+            dev_files.append(f"tf_records_{file_idx[i]}.tfrecordz")
+            # Accumulate the number of examples in each file
+            dev_size += int(size_arr[file_idx[i]])
+
+        print(f"Dev files for evaluation: {dev_files}")
+        print(f"TOTAL NUMBER OF EXAMPLES: {dev_size}")
+
+        dev = tf.compat.v2.data.TFRecordDataset(dev_files, compression_type="GZIP", num_parallel_reads=16)
+        dev = dev.map(parse_proto, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dev = dev.batch(batch_size, drop_remainder=True)
+        dev = dev.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        dev_loss = model.evaluate(dev)[-1]
+
+    ### Predict on all...
+    print(f"Final Metrics: alpha={l_r}, beta_1={b_1}, beta_2={b_2}, batch_size={batch_size}, train_loss={final_loss}, dev_loss={dev_loss}, num_train_samples={num_train_samples}, dev_samples={dev_size}, epochs={epochs}")
+
     test_data = False
+    #test_data = False
 
     if make_test is True:
         if test_data is True:
             create_test_set(raw_path, comp_path, ignore_list, './test_set')
-        total_file = np.load(f"test_set_0.npz")
-        arr_comp = total_file['arr_0']
-        spikes = arr_comp.shape[0]
-        new_arr = np.copy(arr_comp)
-        print(f"This many spikes...{arr_comp.shape}")
-        spike_arr = arr_comp[:,2:]
-        spike_arr = spike_arr[:, None ,:]
-        t = time.time()
-        # do stuff
-        predicted = model.predict(spike_arr, verbose=0)
-        elapsed = time.time() - t
-        print(f"Elapsed time for predictions: {elapsed}")
-        predicted = predicted.reshape((spikes, 71))
-        predicted = ((predicted*1000).astype(int))/1000
-        new_arr[:, 2:] = predicted #np.round(predicted, decimals=2)
-        print("Made it...")
 
-        np.savez_compressed("final_testing_spikes.npz", new_arr)
-        final = np.load("final_testing_spikes.npz")
+        for i in range(11):
+            total_file = np.load(f"test_set_{i}.npz")
+            arr_comp = total_file['arr_0']
+            spikes = arr_comp.shape[0]
+            new_arr = np.copy(arr_comp)
+            print(f"This many spikes...{arr_comp.shape}")
+            spike_arr = arr_comp[:,2:]
+            spike_arr = spike_arr[:, None ,:]
+            t = time.time()
+            # do stuff
+            predicted = model.predict(spike_arr, verbose=0)
+            elapsed = time.time() - t
+            print(f"Elapsed time for predictions: {elapsed}")
+            predicted = predicted.reshape((spikes, 71))
+            predicted = ((predicted*1000).astype(int))/1000
+            new_arr[:, 2:] = predicted #np.round(predicted, decimals=2)
+            savemat(f"final_testing_spikes_{i}.mat", {'spikeRep': new_arr})
+            return
+            #np.savez_compressed(f"final_testing_spikes_{i}.npz", new_arr)
+
+        final = np.load("final_testing_spikes_0.npz")
         final_arr = final['arr_0']
-        #final_arr = ((final_arr*1000).astype(int))/1000
         pic = 0
         print(final_arr[pic, 2:])
         print(f"Shape...{final_arr.shape}")
@@ -522,12 +548,12 @@ def train(remake=False, use_chk=False, make_test=False, plot=False, show=False, 
 if __name__=="__main__":
     train(
         remake=False, # Create new npz
-        use_chk=True, # Use checkpointed model (don't train again)
-        make_test=True,
-        plot=True, # plot the figures at the end
-        show=True, # show any plots
+        use_chk=False, # Use checkpointed model (don't train again)
+        make_test=False,
+        plot=False, # plot the figures at the end
+        show=False, # show any plots
         save=False, # save the images
-        epochs=2,
-        plot_idx=0 # which item to plot
+        epochs=5,
+        plot_idx=0, # which item to plot
+        val=True
     ) 
-
